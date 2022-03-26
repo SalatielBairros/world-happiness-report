89d5a087d09e3fd036263dd15c6b9a85aa5681be
Salatiel Bairros <salatiel.costabairros@gmail.com>
2022-03-26 08:31:11 -0300

    command pattern

--- src/data_preparation_commands.py
+++ src/data_preparation_commands.py
@@ -2,8 +2,11 @@ from data_preparation.clean_columns import CleanColumns
 from data_preparation.region import Region
 from data_preparation.affects import Affects
 from data_preparation.dataset_join import DatasetsJoin
+from commands_handler import Commands
 
-CleanColumns().execute()
-Region().execute()
-Affects().execute()
-DatasetsJoin().execute()
+Commands() \
+    .add_command(CleanColumns) \
+    .add_command(Region) \
+    .add_command(Affects) \
+    .add_command(DatasetsJoin) \
+    .execute()
---
+++ src/lib/commands_handler.py
@@ -0,0 +1,11 @@
+class Commands:
+    def __init__(self) -> None:
+        self.commands = []
+
+    def add_command(self, command):
+        self.commands.append(command)
+        return self
+
+    def execute(self):
+        for command in self.commands:
+            command().execute()
