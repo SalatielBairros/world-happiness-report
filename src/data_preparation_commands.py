f7298d4abf269f957ab5d3f62439d65a49eb7812
Salatiel Bairros <salatiel.costabairros@gmail.com>
2022-03-29 09:50:40 -0300

    missing data command

--- src/data_preparation_commands.py
+++ src/data_preparation_commands.py
@@ -2,11 +2,14 @@ from data_preparation.clean_columns import CleanColumns
 from data_preparation.region import Region
 from data_preparation.affects import Affects
 from data_preparation.dataset_join import DatasetsJoin
+from data_preparation.missing_data import MissingData
 from lib.commands_handler import Commands
 
 Commands() \
-    .add_command(CleanColumns) \
-    .add_command(Region) \
-    .add_command(Affects) \
-    .add_command(DatasetsJoin) \
+    .add_command(MissingData) \
     .execute()
+    # .add_command(CleanColumns) \
+    # .add_command(Region) \
+    # .add_command(Affects) \
+    # .add_command(DatasetsJoin) \
+    
