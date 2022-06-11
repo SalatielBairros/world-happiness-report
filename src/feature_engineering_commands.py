91f157aa151d4593d102861a550c44d4bf75d677
Salatiel Bairros <salatiel.costabairros@gmail.com>
2022-06-11 13:55:47 -0300

    model and data analisys

--- src/feature_engineering_commands.py
+++ src/feature_engineering_commands.py
@@ -1,8 +1,12 @@
 from feature_engineering.cat_country import CatCountry
 from feature_engineering.cat_region import CatRegion
+from feature_engineering.rounded_score import RoundedScore
+from feature_engineering.scaled_hle import ScaledHle
 from lib.commands_handler import Commands
 
 Commands() \
     .add_command(CatRegion) \
     .add_command(CatCountry) \
+    .add_command(RoundedScore) \
+    .add_command(ScaledHle) \
     .execute()
