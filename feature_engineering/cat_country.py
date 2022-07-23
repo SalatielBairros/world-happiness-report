6c03504e5a4fce193e3a75110988e5fc763ede3b
Salatiel Bairros <salatiel.costabairros@gmail.com>
2022-07-23 11:09:00 -0300

    cat country refactoring

--- feature_engineering/cat_country.py
+++ feature_engineering/cat_country.py
@@ -3,12 +3,10 @@ import pandas as pd
 class CatCountry():
     __name__ = 'Cat Country'
 
-    def __init__(self, output_directory = './data/feature_engineering', input_file='./data/feature_engineering/cat_region.csv'):    
-        self.output_directory = output_directory
-        self.input_file = input_file
+    def __init__(self, dataset: pd.DataFrame):    
+        self.dataset = dataset
 
-    def execute(self):
-        dataset = pd.read_csv(self.input_file)
-        dataset['cat_country'] = pd.Categorical(dataset['country'])
-        dataset['cat_country'] = dataset['cat_country'].cat.codes
-        dataset.to_csv(self.output_directory + '/cat_country.csv', index=False)
+    def execute(self):        
+        self.dataset['cat_country'] = pd.Categorical(self.dataset['country'])
+        self.dataset['cat_country'] = self.dataset['cat_country'].cat.codes
+        return self.dataset
