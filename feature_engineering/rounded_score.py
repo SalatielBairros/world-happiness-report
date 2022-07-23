320718058cdb26064dfe99da56a80966dd60307a
Salatiel Bairros <salatiel.costabairros@gmail.com>
2022-07-23 11:09:43 -0300

    rounded score

--- feature_engineering/rounded_score.py
+++ feature_engineering/rounded_score.py
@@ -3,11 +3,9 @@ import pandas as pd
 class RoundedScore():
     __name__ = 'Rounded Score'
 
-    def __init__(self, output_directory = './data/feature_engineering', input_file='./data/feature_engineering/cat_country.csv'):    
-        self.output_directory = output_directory
-        self.input_file = input_file
+    def __init__(self, dataset: pd.DataFrame):
+        self.dataset = dataset
 
-    def execute(self):
-        dataset = pd.read_csv(self.input_file)
-        dataset['rounded_score'] = dataset['score'].round(0)        
-        dataset.to_csv(self.output_directory + '/rounded_score.csv', index=False)
+    def execute(self):        
+        self.dataset['rounded_score'] = self.dataset['score'].round(0)
+        return self.dataset
